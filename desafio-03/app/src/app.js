002a54af2f40584d99c30fc1d444b33bce093af8
dticed <lucaspierrealencar@gmail.com>
2021-09-07 14:55:47 -0300

    conclusao desafio 03

---
+++ desafio-03/app/src/app.js
@@ -0,0 +1,84 @@
+//import Form from "./form";
+import Table from "./table";
+import { useState, useEffect } from "react";
+
+function App() {
+  const [data, setData] = useState([]);
+  console.log("1 executou");
+
+  function getCarros() {
+    fetch("http://localhost:3333/cars")
+      .then((result) => result.json())
+      .then((result) => setData(result));
+  }
+
+  useEffect(() => {
+    console.log("3 executou");
+    getCarros()
+
+    return () => {
+      console.log("clean");
+    };
+  }, []);
+  console.log("2 executou");
+
+  return (
+    <>
+      <Form data={data} getCarros={getCarros} />
+      <Table data={data} getCarros={getCarros} />
+    </>
+  );
+}
+
+function Form({ getCarros }) {
+  function handleForm(e) {
+    e.preventDefault();
+
+    const car = {
+      image: e.target.elements.imagem.value,
+      brandModel: e.target.elements.marca.value,
+      year: e.target.elements.ano.value,
+      plate: e.target.elements.placa.value,
+      color: e.target.elements.cor.value,
+    };
+
+    const requestOption = {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(car),
+    };
+    fetch("http://localhost:3333/cars", requestOption)
+      .then((response) => response.json())
+      .then((response) => {
+        console.log(response)
+        getCarros()
+      })
+
+    
+  }
+
+  return (
+    <section className="grid grid-form">
+      <form onSubmit={handleForm}>
+        <label>Url da Imagem:</label>
+        <input type="text" name="imagem"></input>
+
+        <label>Marca:</label>
+        <input type="text" name="marca"></input>
+
+        <label>Ano:</label>
+        <input type="number" name="ano"></input>
+
+        <label>Placa:</label>
+        <input type="text" name="placa"></input>
+
+        <label>Cor:</label>
+        <input type="text" name="cor"></input>
+
+        <button type="submit">Enviar</button>
+      </form>
+    </section>
+  );
+}
+
+export default App;
---
+++ desafio-03/app/src/form.js
@@ -0,0 +1,32 @@
+// function Form() {
+
+//   function handleForm(e) {
+//     e.preventDefault()
+//     console.log(e.target.elements)
+//   }
+
+//   return(
+//     <section className="grid grid-form">
+//       <form onSubmit={handleForm}>
+//         <label>Url da Imagem:</label>
+//         <input type="text" name="imagem" placeholder="url da imagem"></input>
+
+//         <label>Marca:</label>
+//         <input type="text" name="marca" placeholder="marca do carro"></input>
+
+//         <label>Ano:</label>
+//         <input type="number" name="ano" placeholder="ano do carro"></input>
+
+//         <label>Placa:</label>
+//         <input type="text" name="placa" placeholder="placa do carro"></input>
+
+//         <label>Cor:</label>
+//         <input type="text" name="cor" placeholder="cor do carro"></input>
+
+//         <button type="submit">Enviar</button>
+//       </form>
+//     </section>
+//   )
+// }
+
+// export default Form;
